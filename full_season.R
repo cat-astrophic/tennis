a10b2e94146c01477825c08ba5f22005d75f4517
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2020-10-13 19:16:03 -0400

    logit models for all three tournaments combined

--- full_season.R
+++ full_season.R
@@ -46,6 +46,11 @@ data <- data[which(data$COVID == 0),]
 data <- data[which(data$Injury == 0),]
 data <- data[which(data$Suspension == 0),]
 
+# Creating male and female only data.frames
+
+datam <- data[which(data$Gender == 'M'),]
+dataf <- data[which(data$Gender == 'F'),]
+
 # Running the (logit) models for all players
 
 # Model 1 is just regressing gender on participation
@@ -89,6 +94,8 @@ mod9 <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_2
 # Subsetting data for singles players who were not 'substitutes' only
 
 subdata <- data[which(data$Lucky == 0),]
+subdatam <- datam[which(data$Lucky == 0),]
+subdataf <- dataf[which(data$Lucky == 0),]
 
 # Model 1 is just regressing gender on participation
 
@@ -200,4 +207,228 @@ write.csv(stargazer(submod1, submod2, submod3, submod4, submod5, submod6, submod
 stargazer(mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8, mod9, type = 'text', se = list(rse1, rse2, rse3, rse4, rse5, rse6, rse7, rse8, rse9))
 stargazer(submod1, submod2, submod3, submod4, submod5, submod6, submod7, submod8, submod9, type = 'text',
           se = list(subrse1, subrse2, subrse3, subrse4, subrse5, subrse6, subrse7, subrse8, subrse9))
+
+# Repeat all regressions for women and men only
+
+mod2m <- glm(Competed ~ Age, family = binomial(link = logit), data = datam)
+
+mod3m <- glm(Competed ~ Age + log(Winnings + 1), family = binomial(link = logit), data = datam)
+
+mod4m <- glm(Competed ~ Age + log(Winnings_20 + 1), family = binomial(link = logit), data = datam)
+
+mod5m <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1), family = binomial(link = logit), data = datam)
+
+mod6m <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+             + Titles + Majors + Current + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = datam)
+
+mod7m <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+             + Titles + Majors_Bi + Current + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = datam)
+
+mod8m <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+             + Titles + Majors + Current_Bi + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = datam)
+
+mod9m <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+             + Titles + Majors_Bi + Current_Bi + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = datam)
+
+mod2f <- glm(Competed ~ Age, family = binomial(link = logit), data = dataf)
+
+mod3f <- glm(Competed ~ Age + log(Winnings + 1), family = binomial(link = logit), data = dataf)
+
+mod4f <- glm(Competed ~ Age + log(Winnings_20 + 1), family = binomial(link = logit), data = dataf)
+
+mod5f <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1), family = binomial(link = logit), data = dataf)
+
+mod6f <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+             + Titles + Majors + Current + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = dataf)
+
+mod7f <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+             + Titles + Majors_Bi + Current + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = dataf)
+
+mod8f <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+             + Titles + Majors + Current_Bi + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = dataf)
+
+mod9f <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+             + Titles + Majors_Bi + Current_Bi + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = dataf)
+
+submod2m <- glm(Competed ~ Age, family = binomial(link = logit), data = subdatam)
+
+submod3m <- glm(Competed ~ Age + log(Winnings + 1), family = binomial(link = logit), data = subdatam)
+
+submod4m <- glm(Competed ~ Age + log(Winnings_20 + 1), family = binomial(link = logit), data = subdatam)
+
+submod5m <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1), family = binomial(link = logit), data = subdatam)
+
+submod6m <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+                + Titles + Majors + Current + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = subdatam)
+
+submod7m <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+                + Titles + Majors_Bi + Current + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = subdatam)
+
+submod8m <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+                + Titles + Majors + Current_Bi + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = subdatam)
+
+submod9m <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+                + Titles + Majors_Bi + Current_Bi + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = subdatam)
+
+submod2f <- glm(Competed ~ Age, family = binomial(link = logit), data = subdataf)
+
+submod3f <- glm(Competed ~ Age + log(Winnings + 1), family = binomial(link = logit), data = subdataf)
+
+submod4f <- glm(Competed ~ Age + log(Winnings_20 + 1), family = binomial(link = logit), data = subdataf)
+
+submod5f <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1), family = binomial(link = logit), data = subdataf)
+
+submod6f <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+                + Titles + Majors + Current + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = subdataf)
+
+submod7f <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+                + Titles + Majors_Bi + Current + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = subdataf)
+
+submod8f <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+                + Titles + Majors + Current_Bi + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = subdataf)
+
+submod9f <- glm(Competed ~ Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+                + Titles + Majors_Bi + Current_Bi + factor(Event.Type) + Prev_Tourn_Comp, family = binomial(link = logit), data = subdataf)
+
+# Generating  heteroskedasticity robust standard errors
+
+cov <- vcovHC(mod2m, type = 'HC0')
+rse2m <- sqrt(diag(cov))
+
+cov <- vcovHC(mod3m, type = 'HC0')
+rse3m <- sqrt(diag(cov))
+
+cov <- vcovHC(mod4m, type = 'HC0')
+rse4m <- sqrt(diag(cov))
+
+cov <- vcovHC(mod5m, type = 'HC0')
+rse5m <- sqrt(diag(cov))
+
+cov <- vcovHC(mod6m, type = 'HC0')
+rse6m <- sqrt(diag(cov))
+
+cov <- vcovHC(mod7m, type = 'HC0')
+rse7m <- sqrt(diag(cov))
+
+cov <- vcovHC(mod8m, type = 'HC0')
+rse8m <- sqrt(diag(cov))
+
+cov <- vcovHC(mod9m, type = 'HC0')
+rse9m <- sqrt(diag(cov))
+
+cov <- vcovHC(mod2f, type = 'HC0')
+rse2f <- sqrt(diag(cov))
+
+cov <- vcovHC(mod3f, type = 'HC0')
+rse3f <- sqrt(diag(cov))
+
+cov <- vcovHC(mod4f, type = 'HC0')
+rse4f <- sqrt(diag(cov))
+
+cov <- vcovHC(mod5f, type = 'HC0')
+rse5f <- sqrt(diag(cov))
+
+cov <- vcovHC(mod6f, type = 'HC0')
+rse6f <- sqrt(diag(cov))
+
+cov <- vcovHC(mod7f, type = 'HC0')
+rse7f <- sqrt(diag(cov))
+
+cov <- vcovHC(mod8f, type = 'HC0')
+rse8f <- sqrt(diag(cov))
+
+cov <- vcovHC(mod9f, type = 'HC0')
+rse9f <- sqrt(diag(cov))
+
+cov <- vcovHC(submod2m, type = 'HC0')
+subrse2m <- sqrt(diag(cov))
+
+cov <- vcovHC(submod3m, type = 'HC0')
+subrse3m <- sqrt(diag(cov))
+
+cov <- vcovHC(submod4m, type = 'HC0')
+subrse4m <- sqrt(diag(cov))
+
+cov <- vcovHC(submod5m, type = 'HC0')
+subrse5m <- sqrt(diag(cov))
+
+cov <- vcovHC(submod6m, type = 'HC0')
+subrse6m <- sqrt(diag(cov))
+
+cov <- vcovHC(submod7m, type = 'HC0')
+subrse7m <- sqrt(diag(cov))
+
+cov <- vcovHC(submod8m, type = 'HC0')
+subrse8m <- sqrt(diag(cov))
+
+cov <- vcovHC(submod9m, type = 'HC0')
+subrse9m <- sqrt(diag(cov))
+
+cov <- vcovHC(submod2f, type = 'HC0')
+subrse2f <- sqrt(diag(cov))
+
+cov <- vcovHC(submod3f, type = 'HC0')
+subrse3f <- sqrt(diag(cov))
+
+cov <- vcovHC(submod4f, type = 'HC0')
+subrse4f <- sqrt(diag(cov))
+
+cov <- vcovHC(submod5f, type = 'HC0')
+subrse5f <- sqrt(diag(cov))
+
+cov <- vcovHC(submod6f, type = 'HC0')
+subrse6f <- sqrt(diag(cov))
+
+cov <- vcovHC(submod7f, type = 'HC0')
+subrse7f <- sqrt(diag(cov))
+
+cov <- vcovHC(submod8f, type = 'HC0')
+subrse8f <- sqrt(diag(cov))
+
+cov <- vcovHC(submod9f, type = 'HC0')
+subrse9f <- sqrt(diag(cov))
+
+# Writing results ot file
+
+write.csv(stargazer(mod2m, mod3m, mod4m, mod5m, mod6m, mod7m, mod8m, mod9m, type = 'text',
+                    se = list(rse2m, rse3m, rse4m, rse5m, rse6m, rse7m, rse8m, rse9m)),
+          paste(directory, 'results_all_full_season_M.txt', sep = ''), row.names = FALSE)
+write.csv(stargazer(submod2m, submod3m, submod4m, submod5m, submod6m, submod7m, submod8m, submod9m, type = 'text',
+                    se = list(subrse2m, subrse3m, subrse4m, subrse5m, subrse6m, subrse7m, subrse8m, subrse9m)),
+          paste(directory, 'results_lucky_full_season_M.txt', sep = ''), row.names = FALSE)
+
+write.csv(stargazer(mod2m, mod3m, mod4m, mod5m, mod6m, mod7m, mod8m, mod9m,
+                    se = list(rse2m, rse3m, rse4m, rse5m, rse6m, rse7m, rse8m, rse9m)),
+          paste(directory, 'results_all_full_season_M_tex.txt', sep = ''), row.names = FALSE)
+write.csv(stargazer(submod2m, submod3m, submod4m, submod5m, submod6m, submod7m, submod8m, submod9m,
+                    se = list(subrse2m, subrse3m, subrse4m, subrse5m, subrse6m, subrse7m, subrse8m, subrse9m)),
+          paste(directory, 'results_lucky_full_season_M_tex.txt', sep = ''), row.names = FALSE)
+
+write.csv(stargazer(mod2f, mod3f, mod4f, mod5f, mod6f, mod7f, mod8f, mod9f, type = 'text',
+                    se = list(rse2f, rse3f, rse4f, rse5f, rse6f, rse7f, rse8f, rse9f)),
+          paste(directory, 'results_all_full_season_F.txt', sep = ''), row.names = FALSE)
+write.csv(stargazer(submod2f, submod3f, submod4f, submod5f, submod6f, submod7f, submod8f, submod9f, type = 'text',
+                    se = list(subrse2f, subrse3f, subrse4f, subrse5f, subrse6f, subrse7f, subrse8f, subrse9f)),
+          paste(directory, 'results_lucky_full_season_F.txt', sep = ''), row.names = FALSE)
+
+write.csv(stargazer(mod2f, mod3f, mod4f, mod5f, mod6f, mod7f, mod8f, mod9f,
+                    se = list(rse2f, rse3f, rse4f, rse5f, rse6f, rse7f, rse8f, rse9f)),
+          paste(directory, 'results_all_full_season_F_tex.txt', sep = ''), row.names = FALSE)
+write.csv(stargazer(submod2f, submod3f, submod4f, submod5f, submod6f, submod7f, submod8f, submod9f,
+                    se = list(subrse2f, subrse3f, subrse4f, subrse5f, subrse6f, subrse7f, subrse8f, subrse9f)),
+          paste(directory, 'results_lucky_full_season_F_tex.txt', sep = ''), row.names = FALSE)
+
+stargazer(mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8, mod9, type = 'text', se = list(rse1, rse2, rse3, rse4, rse5, rse6, rse7, rse8, rse9))
+stargazer(submod1, submod2, submod3, submod4, submod5, submod6, submod7, submod8, submod9, type = 'text',
+          se = list(subrse1, subrse2, subrse3, subrse4, subrse5, subrse6, subrse7, subrse8, subrse9))
+
+stargazer(mod2m, mod3m, mod4m, mod5m, mod6m, mod7m, mod8m, mod9m, type = 'text',
+          se = list(rse2m, rse3m, rse4m, rse5m, rse6m, rse7m, rse8m, rse9m))
+stargazer(submod2m, submod3m, submod4m, submod5m, submod6m, submod7m, submod8m, submod9m, type = 'text',
+          se = list(subrse2m, subrse3m, subrse4m, subrse5m, subrse6m, subrse7m, subrse8m, subrse9m))
+
+stargazer(mod2f, mod3f, mod4f, mod5f, mod6f, mod7f, mod8f, mod9f, type = 'text',
+          se = list(rse2f, rse3f, rse4f, rse5f, rse6f, rse7f, rse8f, rse9f))
+stargazer(submod2f, submod3f, submod4f, submod5f, submod6f, submod7f, submod8f, submod9f, type = 'text',
+          se = list(subrse2f, subrse3f, subrse4f, subrse5f, subrse6f, subrse7f, subrse8f, subrse9f))
 
