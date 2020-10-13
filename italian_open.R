cf64380d293a0bcc9f2951bf1866429d136d4ceb
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2020-10-12 20:35:31 -0400

    logit models for italian open only

--- italian_open.R
+++ italian_open.R
@@ -11,6 +11,12 @@ library(ggplot2)
 
 data <- read.csv(paste(directory, 'italian.csv', sep = ''), fileEncoding = 'UTF-8-BOM')
 
+# Dropping individuals who did not choose whether or not to participate, i.e., those who had COVID, an injury, or were suspended
+
+data <- data[which(data$COVID == 0),]
+data <- data[which(data$Injury == 0),]
+data <- data[which(data$Suspension == 0),]
+
 # Running the (logit) models for all players
 
 # Model 1 is just regressing gender on participation
@@ -23,7 +29,7 @@ mod2 <- glm(Competed ~ factor(Gender) + Age, family = binomial(link = logit), da
 
 # Model 3 is gender + age + career earnings
 
-mod3 <- glm(Competed ~ factor(Gender) + Age + log(Winnings), family = binomial(link = logit), data = data)
+mod3 <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1), family = binomial(link = logit), data = data)
 
 # Model 4 is gender + age + YTD earnings
 
@@ -31,23 +37,37 @@ mod4 <- glm(Competed ~ factor(Gender) + Age + log(Winnings_20 + 1), family = bin
 
 # Model 5 is gender + age + career earnings + YTD earnings
 
-mod5 <- glm(Competed ~ factor(Gender) + Age + log(Winnings) + log(Winnings_20 + 1), family = binomial(link = logit), data = data)
+mod5 <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1), family = binomial(link = logit), data = data)
 
 # Model 6 is Model 5 + remaining controls + singles ranking
 
-mod6 <- glm(Competed ~ factor(Gender) + Age + log(Winnings) + log(Winnings_20 + 1) + Doubles + COVID + Suspension + Qualifier
-            + Injury + Ranking_S + Titles + Majors + Italian_Open + Prev_Tourn_Res + Prev_Tourn_Comp, family = binomial(link = logit), data = data)
+mod6 <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+            + Titles + Majors + Italian_Open, family = binomial(link = logit), data = data)
 
 # Models 7-9 are Model 6 with alternative specifications for Majors and Italian_Open to serve as robustness checks
 
-mod7 <- glm(Competed ~ factor(Gender) + Age + log(Winnings) + log(Winnings_20 + 1) + Doubles + COVID + Suspension + Qualifier 
-            + Injury + Ranking_S + Titles + Majors_Bi + Italian_Open + Prev_Tourn_Res + Prev_Tourn_Comp, family = binomial(link = logit), data = data)
+mod7 <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+            + Titles + Majors_Bi + Italian_Open, family = binomial(link = logit), data = data)
 
-mod8 <- glm(Competed ~ factor(Gender) + Age + log(Winnings) + log(Winnings_20 + 1) + Doubles + COVID + Suspension + Qualifier
-            + Injury + Ranking_S + Titles + Majors + Italian_Open_Bi + Prev_Tourn_Res + Prev_Tourn_Comp, family = binomial(link = logit), data = data)
+mod8 <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+            + Titles + Majors + Italian_Open_Bi, family = binomial(link = logit), data = data)
 
-mod9 <- glm(Competed ~ factor(Gender) + Age + log(Winnings) + log(Winnings_20 + 1) + Doubles + COVID + Suspension + Qualifier
-            + Injury + Ranking_S + Titles + Majors_Bi + Italian_Open_Bi + Prev_Tourn_Res + Prev_Tourn_Comp, family = binomial(link = logit), data = data)
+mod9 <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+            + Titles + Majors_Bi + Italian_Open_Bi, family = binomial(link = logit), data = data)
+
+# Running models 6-9 with Doubles dropped to prevent the error of quasi-complete data separation
+
+mod6d <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Qualifier + Ranking_S
+             + Titles + Majors + Italian_Open, family = binomial(link = logit), data = data)
+
+mod7d <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Qualifier + Ranking_S
+             + Titles + Majors_Bi + Italian_Open, family = binomial(link = logit), data = data)
+
+mod8d <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Qualifier + Ranking_S
+             + Titles + Majors + Italian_Open_Bi, family = binomial(link = logit), data = data)
+
+mod9d <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Qualifier + Ranking_S
+             + Titles + Majors_Bi + Italian_Open_Bi, family = binomial(link = logit), data = data)
 
 # Running the (logit) models for singles players who were not 'substitutes' only
 
@@ -65,7 +85,7 @@ submod2 <- glm(Competed ~ factor(Gender) + Age, family = binomial(link = logit),
 
 # Model 3 is gender + age + career earnings
 
-submod3 <- glm(Competed ~ factor(Gender) + Age + log(Winnings), family = binomial(link = logit), data = subdata)
+submod3 <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1), family = binomial(link = logit), data = subdata)
 
 # Model 4 is gender + age + YTD earnings
 
@@ -73,23 +93,37 @@ submod4 <- glm(Competed ~ factor(Gender) + Age + log(Winnings_20 + 1), family = 
 
 # Model 5 is gender + age + career earnings + YTD earnings
 
-submod5 <- glm(Competed ~ factor(Gender) + Age + log(Winnings) + log(Winnings_20 + 1), family = binomial(link = logit), data = subdata)
+submod5 <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1), family = binomial(link = logit), data = subdata)
 
 # Model 6 is Model 5 + remaining controls + singles ranking
 
-submod6 <- glm(Competed ~ factor(Gender) + Age + log(Winnings) + log(Winnings_20 + 1) + Doubles + COVID + Suspension + Qualifier
-               + Injury + Ranking_S + Titles + Majors + Italian_Open + Prev_Tourn_Res + Prev_Tourn_Comp, family = binomial(link = logit), data = subdata)
+submod6 <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+               + Titles + Majors + Italian_Open, family = binomial(link = logit), data = subdata)
 
 # Models 7-9 are Model 6 with alternative specifications for Majors and Italian_Open to serve as robustness checks
 
-submod7 <- glm(Competed ~ factor(Gender) + Age + log(Winnings) + log(Winnings_20 + 1) + Doubles + COVID + Suspension + Qualifier 
-               + Injury + Ranking_S + Titles + Majors_Bi + Italian_Open + Prev_Tourn_Res + Prev_Tourn_Comp, family = binomial(link = logit), data = subdata)
+submod7 <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+               + Titles + Majors_Bi + Italian_Open, family = binomial(link = logit), data = subdata)
 
-submod8 <- glm(Competed ~ factor(Gender) + Age + log(Winnings) + log(Winnings_20 + 1) + Doubles + COVID + Suspension + Qualifier
-               + Injury + Ranking_S + Titles + Majors + Italian_Open_Bi + Prev_Tourn_Res + Prev_Tourn_Comp, family = binomial(link = logit), data = subdata)
+submod8 <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+               + Titles + Majors + Italian_Open_Bi, family = binomial(link = logit), data = subdata)
 
-submod9 <- glm(Competed ~ factor(Gender) + Age + log(Winnings) + log(Winnings_20 + 1) + Doubles + COVID + Suspension + Qualifier
-               + Injury + Ranking_S + Titles + Majors_Bi + Italian_Open_Bi + Prev_Tourn_Res + Prev_Tourn_Comp, family = binomial(link = logit), data = subdata)
+submod9 <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Doubles + Qualifier + Ranking_S
+               + Titles + Majors_Bi + Italian_Open_Bi, family = binomial(link = logit), data = subdata)
+
+# Running models 6-9 with Doubles dropped to prevent the error of quasi-complete data separation
+
+submod6d <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Qualifier + Ranking_S
+                + Titles + Majors + Italian_Open, family = binomial(link = logit), data = subdata)
+
+submod7d <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Qualifier + Ranking_S
+                + Titles + Majors_Bi + Italian_Open, family = binomial(link = logit), data = subdata)
+
+submod8d <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Qualifier + Ranking_S
+                + Titles + Majors + Italian_Open_Bi, family = binomial(link = logit), data = subdata)
+
+submod9d <- glm(Competed ~ factor(Gender) + Age + log(Winnings + 1) + log(Winnings_20 + 1) + Qualifier + Ranking_S
+                + Titles + Majors_Bi + Italian_Open_Bi, family = binomial(link = logit), data = subdata)
 
 # Generating  heteroskedasticity robust standard errors
 
@@ -120,6 +154,18 @@ rse8 <- sqrt(diag(cov))
 cov <- vcovHC(mod9, type = 'HC0')
 rse9 <- sqrt(diag(cov))
 
+cov <- vcovHC(mod6d, type = 'HC0')
+rse6d <- sqrt(diag(cov))
+
+cov <- vcovHC(mod7d, type = 'HC0')
+rse7d <- sqrt(diag(cov))
+
+cov <- vcovHC(mod8d, type = 'HC0')
+rse8d <- sqrt(diag(cov))
+
+cov <- vcovHC(mod9d, type = 'HC0')
+rse9d <- sqrt(diag(cov))
+
 cov <- vcovHC(submod1, type = 'HC0')
 subrse1 <- sqrt(diag(cov))
 
@@ -147,59 +193,55 @@ subrse8 <- sqrt(diag(cov))
 cov <- vcovHC(submod9, type = 'HC0')
 subrse9 <- sqrt(diag(cov))
 
+cov <- vcovHC(submod6d, type = 'HC0')
+subrse6d <- sqrt(diag(cov))
+
+cov <- vcovHC(submod7d, type = 'HC0')
+subrse7d <- sqrt(diag(cov))
+
+cov <- vcovHC(submod8d, type = 'HC0')
+subrse8d <- sqrt(diag(cov))
+
+cov <- vcovHC(submod9d, type = 'HC0')
+subrse9d <- sqrt(diag(cov))
+
 # Viewing regression results and saving to file
 
-stargazer(mod1, mod2, mod3, mod4, mod5, mod6, type = 'text', se = list(rse1, rse2, rse3, rse4, rse5, rse6))
-stargazer(submod1, submod2, submod3, submod4, submod5, submod6, type = 'text', se = list(subrse1, subrse2, subrse3, subrse4, subrse5, subrse6))
+stargazer(mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8, mod9, mod6d, mod7d, mod8d, mod9d, type = 'text',
+          se = list(rse1, rse2, rse3, rse4, rse5, rse6, rse7, rse8, rse9, rse6d, rse7d, rse8d, rse9d))
+stargazer(submod1, submod2, submod3, submod4, submod5, submod6, submod7, submod8, submod9, submod6d, submod7d, submod8d, submod9d, type = 'text',
+          se = list(subrse1, subrse2, subrse3, subrse4, subrse5, subrse6, subrse7, subrse8, subrse9, subrse6d, subrse7d, subrse8d, subrse9d))
 
-write.csv(stargazer(mod1, mod2, mod3, mod4, mod5, mod6, type = 'text',
-                    se = list(rse1, rse2, rse3, rse4, rse5, rse6)),
+write.csv(stargazer(mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8, mod9, type = 'text',
+                    se = list(rse1, rse2, rse3, rse4, rse5, rse6, rse7, rse8, rse9)),
           paste(directory, 'results_all_italian.txt', sep = ''), row.names = FALSE)
-write.csv(stargazer(submod1, submod2, submod3, submod4, submod5, submod6, type = 'text',
-                    se = list(subrse1, subrse2, subrse3, subrse4, subrse5, subrse6)),
+write.csv(stargazer(submod1, submod2, submod3, submod4, submod5, submod6, submod7, submod8, submod9, type = 'text',
+                    se = list(subrse1, subrse2, subrse3, subrse4, subrse5, subrse6, subrse7, subrse8, subrse9)),
           paste(directory, 'results_lucky_italian.txt', sep = ''), row.names = FALSE)
 
-write.csv(stargazer(mod1, mod2, mod3, mod4, mod5, mod6,
-                    se = list(rse1, rse2, rse3, rse4, rse5, rse6)),
+write.csv(stargazer(mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8, mod9,
+                    se = list(rse1, rse2, rse3, rse4, rse5, rse6, rse7, rse8, rse9)),
           paste(directory, 'results_all_italian_tex.txt', sep = ''), row.names = FALSE)
-write.csv(stargazer(submod1, submod2, submod3, submod4, submod5, submod6,
-                    se = list(subrse1, subrse2, subrse3, subrse4, subrse5, subrse6)),
+write.csv(stargazer(submod1, submod2, submod3, submod4, submod5, submod6, submod7, submod8, submod9,
+                    se = list(subrse1, subrse2, subrse3, subrse4, subrse5, subrse6, subrse7, subrse8, subrse9)),
           paste(directory, 'results_lucky_italian_tex.txt', sep = ''), row.names = FALSE)
 
-# Viewing the results from the robustness checks
+write.csv(stargazer(mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8, mod9, mod6d, mod7d, mod8d, mod9d, type = 'text',
+                    se = list(rse1, rse2, rse3, rse4, rse5, rse6, rse7, rse8, rse9, rse6d, rse7d, rse8d, rse9d)),
+          paste(directory, 'results_all_italian_2.txt', sep = ''), row.names = FALSE)
+write.csv(stargazer(submod1, submod2, submod3, submod4, submod5, submod6, submod7, submod8, submod9, submod6d, submod7d, submod8d, submod9d, type = 'text',
+                    se = list(subrse1, subrse2, subrse3, subrse4, subrse5, subrse6, subrse7, subrse8, subrse9, subrse6d, subrse7d, subrse8d, subrse9d)),
+          paste(directory, 'results_lucky_italian_2.txt', sep = ''), row.names = FALSE)
 
-stargazer(mod7, mod8, mod9, type = 'text', se = list(rse7, rse8, rse9))
-stargazer(submod7, submod8, submod9, type = 'text', se = list(subrse7, subrse8, subrse9))
+write.csv(stargazer(mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8, mod9, mod6d, mod7d, mod8d, mod9d,
+                    se = list(rse1, rse2, rse3, rse4, rse5, rse6, rse7, rse8, rse9, rse6d, rse7d, rse8d, rse9d)),
+          paste(directory, 'results_all_italian_tex_2.txt', sep = ''), row.names = FALSE)
+write.csv(stargazer(submod1, submod2, submod3, submod4, submod5, submod6, submod7, submod8, submod9, submod6d, submod7d, submod8d, submod9d,
+                    se = list(subrse1, subrse2, subrse3, subrse4, subrse5, subrse6, subrse7, subrse8, subrse9, subrse6d, subrse7d, subrse8d, subrse9d)),
+          paste(directory, 'results_lucky_italian_tex_2.txt', sep = ''), row.names = FALSE)
 
-# Calculating the marginal effects
-
-marg1 <- margins(mod1, type = 'response')
-marg2 <- margins(mod2, type = 'response')
-marg3 <- margins(mod3, type = 'response')
-marg4 <- margins(mod4, type = 'response')
-marg5 <- margins(mod5, type = 'response')
-marg6 <- margins(mod6, type = 'response')
-
-submarg1 <- margins(submod1, type = 'response')
-submarg2 <- margins(submod2, type = 'response')
-submarg3 <- margins(submod3, type = 'response')
-submarg4 <- margins(submod4, type = 'response')
-submarg5 <- margins(submod5, type = 'response')
-submarg6 <- margins(submod6, type = 'response')
-
-# View all marginal effects
-
-marg1
-marg2
-marg3
-marg4
-marg5
-marg6
-
-submarg1
-submarg2
-submarg3
-submarg4
-submarg5
-submarg6
+stargazer(mod1, mod2, mod3, mod4, mod5, mod6, mod7, mod8, mod9, type = 'text',
+          se = list(rse1, rse2, rse3, rse4, rse5, rse6, rse7, rse8, rse9))
+stargazer(submod1, submod2, submod3, submod4, submod5, submod6, submod7, submod8, submod9, type = 'text',
+          se = list(subrse1, subrse2, subrse3, subrse4, subrse5, subrse6, subrse7, subrse8, subrse9))
 
